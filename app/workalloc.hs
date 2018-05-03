00974148a7bbc524299a9f4db6c311f015f098be
Nathan Ingle <elgni.nahtan@gmail.com>
2018-05-04 05:19:19 +1000

    Calculate intervals

--- app/workalloc.hs
+++ app/workalloc.hs
@@ -9,7 +9,16 @@ Portability : non-portable
 
 Visualise allotment of time for a work day.
 -}
+import           Control.Monad      ((<=<))
+import           Data.List          (groupBy, sortBy)
+import           Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import           Data.Ord           (comparing)
 import           Data.Time
+
+
+type AllocStart = (String, LocalTime)
+type TimeSpent = (String, NominalDiffTime)
 
 
 -- | Parse a single line of input.  The format for each input line is:
@@ -17,7 +26,7 @@ import           Data.Time
 -- > "YYYY-mm-dd HHMM\twhat\tcomment\n"
 --
 -- where @what@ is the description of this allocation, and @comment@ is discarded.
-readLine :: String -> Maybe (String, LocalTime)
+readLine :: String -> Maybe AllocStart
 readLine str =
   case span (/= '\t') str of
     (timestr, '\t' : str') ->
@@ -25,8 +34,36 @@ readLine str =
       in sequence (takeWhile (/= '\t') str', maybeTimestamp)
     _ -> Nothing
 
+-- | Parse lines of input; c.f. 'readLine'.
+readLines :: String -> Maybe (NonEmpty AllocStart)
+readLines = NE.nonEmpty <=< mapM readLine . lines
+
+
+-- | Given a list of events that each describe the start of a piece of work,
+-- determine the duration of each piece of work.
+makeIntervals :: NonEmpty AllocStart -> [TimeSpent]
+makeIntervals events = zipWith go (NE.init events) (NE.tail events)
+  where
+    go (what, starttime) (_, endtime) = (what, diffLocalTime endtime starttime)
+    -- Lifted from @time-1.9@.
+    diffLocalTime a b = diffUTCTime (localTimeToUTC utc a) (localTimeToUTC utc b)
+
+-- | Aggregate time spent per work description.
+tallyIntervals :: [TimeSpent] -> [TimeSpent]
+tallyIntervals = map (sumIntervals . NE.fromList) . groupBy (equating fst) . sortBy (comparing fst)
+
+-- | Test equality after applying a function (by analogy to 'comparing').
+equating :: Eq a => (b -> a) -> b -> b -> Bool
+equating f x y = f x == f y
+
+-- | Note that we don't check that the descriptions all match; we just take the first one.
+sumIntervals :: NonEmpty TimeSpent -> TimeSpent
+sumIntervals intervals = (what, sum (NE.map snd intervals))
+  where
+    what = fst (NE.head intervals)
+
 
 main :: IO ()
 main = do
-  Just events <- (mapM readLine . lines) <$> readFile "allocation.txt"
-  mapM_ print events
+  Just events <- readLines <$> readFile "allocation.txt"
+  mapM_ print $ tallyIntervals $ filter (\x -> fst x /= "Out") $ makeIntervals events
